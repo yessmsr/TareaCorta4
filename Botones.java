4497f123c475a5be6ca8c9ffdeafe3a74ca66997
Yess Solano <90736001+yessmsr@users.noreply.github.com>
2022-04-06 19:27:23 -0600

    Update Botones.java

--- Botones.java
+++ Botones.java
@@ -5,47 +5,56 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import java.awt.*;
 
 public class Botones implements ActionListener {
     JFrame ventana;
 
-    JButton botonMensaje, boton1, boton2, boton3;
-    JPanel panelBotones,panel2;
+    JButton boton1, boton2, boton3;
+    JButton []arrBotones = new JButton[3];
 
     public Botones(){
         ventana = new JFrame("Tarea corta 4"); 
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         agregar();
-        ventana.setPreferredSize(new Dimension(400,400));
+        ventana.setPreferredSize(new Dimension(500,300));
         ventana.pack(); 
         ventana.setVisible(true);  
     }
     public void agregar(){
-        botonMensaje = new JButton("Presione los botones en orden númerico");
-        botonMensaje.setBackground(new Color( 102, 153, 102));
-        botonMensaje.addActionListener(this);
-        boton1 = new JButton("Botón 1");
-        boton1.addActionListener(this);
-        boton2 = new JButton("Botón 2");
-        boton2.addActionListener(this);
-        boton3 = new JButton("Botón 3");
-        boton3.addActionListener(this);
-
-        panelBotones = new JPanel();
-        panelBotones.setLayout(new GridLayout(10, 1));
-
-        panelBotones.add(botonMensaje);
-        panelBotones.add(boton1);
-        panelBotones.add(boton2);
-        panelBotones.add(boton3);
-        ventana.add(panelBotones, BorderLayout.CENTER);
-
+        JLabel mensaje = new JLabel();
+        mensaje.setText( "Toque los botones en orden numerico." );
+        mensaje.setForeground(Color.red);
+        ventana.add(mensaje);
+        ventana.setLayout(new GridLayout(3,3,3,1));
+        for (int i=0;i<arrBotones.length;++i){
+            arrBotones[i]=new JButton("Botón "+(1+i));
+            arrBotones[i].addActionListener(this);
+            ventana.add(arrBotones[i]);
+        }
     }
     @Override
     public void actionPerformed(ActionEvent e){
-        
+        boton1=arrBotones[0];
+        boton2=arrBotones[1];
+        boton3=arrBotones[2];
+        for (int i=0;i<arrBotones.length;++i){
+            if((arrBotones[0]==boton1)&&(i==0)){
+                if(e.getSource().equals(boton1)){
+                    System. out. println("Muy bien");
+                }
+            }
+            if((arrBotones[1]==boton2)&&(i==1)){
+                if(e.getSource().equals(boton2)){
+                    System. out. println("Sigue así");
+                }
+            }
+            if((arrBotones[2]==boton3)&&(i==2)){
+                if(e.getSource().equals(boton3)){
+                    System. out. println("Lo hiciste");
+                }
+            }
+        }
         try {
             for(int i=0; i<=2;i++){
                 if((i==0)&& (!e.getSource().equals(boton1)) ){
@@ -57,12 +66,7 @@ public class Botones implements ActionListener {
                 else if((i==2)&& (!e.getSource().equals(boton3)) ){
                     System. out. println("ERROR");
                 }
-                else{
-                    System. out. println("Tocaste todos los botones bien");
-                }
-            
-            }
-            
+            }  
         } catch (Exception error) {
             JDialog msj = new JDialog();
             msj.setVisible(true); 
@@ -70,7 +74,6 @@ public class Botones implements ActionListener {
             JLabel lab = new JLabel("ERROR. Tocaste mal los botones");
             msj.add(lab);
             msj.setDefaultCloseOperation(JDialog.EXIT_ON_CLOSE);
-        }
-        
+        } 
     }
 }
